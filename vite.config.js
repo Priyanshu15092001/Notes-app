e58386a9f21a804ab0a74b88741a7c7b6f74da71
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-02-28 11:17:22 +0530

    Fixed routing issue in Vercel via vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '0.0.0.0',
-  // },
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // This ensures React Router works on Vercel
+  },
 });
