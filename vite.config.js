8aa4d6eee93540cf215cd91fb44d730b7c02fca0
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-02-28 11:21:59 +0530

    Added vercel.json for correct routing

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true, // This ensures React Router works on Vercel
-  },
+  // server: {
+  //   host: '0.0.0.0',
+  // },
 });
